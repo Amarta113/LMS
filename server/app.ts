8a8ae3934ac455f161f9b29a133ebfe54ee4159f
Amarta113 <amartawaghani@gmail.com>
2026-08-23 17:42:05 +0500

    add: integrate ErrorMiddleware for centralized error handling

--- server/app.ts
+++ server/app.ts
@@ -2,6 +2,7 @@ import express, { NextFunction, Request, Response } from 'express'
 import 'dotenv/config'
 import cors from 'cors';
 import cookieParser from 'cookie-parser'
+import ErrorMiddleware from './middleware/error';
 
 export const app = express()
 
@@ -25,3 +26,5 @@ app.use((req:Request, res:Response, next:NextFunction) => {
     err.statusCode = 404;
     next(err)
 })
+
+app.use(ErrorMiddleware())
