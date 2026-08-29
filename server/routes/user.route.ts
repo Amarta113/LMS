5327e1658c4b825c41e3dc489fa6c96a294ad07b
Amarta113 <amartawaghani@gmail.com>
2026-08-29 22:23:51 +0500

    add: create user route for registration, activation, login, and logout

---
+++ server/routes/user.route.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+import { activateUser, loginUser, logoutUser, registerationUser } from '../controller/user.controller'
+import { isAuthenticated } from '../middleware/auth'
+
+const userRouter = express.Router()
+
+userRouter.post('/registration', registerationUser)
+userRouter.post('/activate-user', activateUser)
+userRouter.post('/login', loginUser)
+userRouter.get('/logout', isAuthenticated, logoutUser)
+
+export default userRouter;
