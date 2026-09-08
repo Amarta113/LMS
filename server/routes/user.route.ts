ffa5b831026fc9cdb677e6f0ea2419736ade57dc
Amarta113 <amartawaghani@gmail.com>
2026-09-08 21:46:10 +0500

    add: include updatePassword and updateProfilePicture routes in user router

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { activateUser, getUserInfo, loginUser, logoutUser, registerationUser, socialAuth, updateAccessToken, updateUserInfo } from '../controller/user.controller'
+import { activateUser, getUserInfo, loginUser, logoutUser, registerationUser, socialAuth, updateAccessToken, updatePassword, updateProfilePicture, updateUserInfo } from '../controller/user.controller'
 import { isAuthenticated } from '../middleware/auth'
 
 const userRouter = express.Router()
@@ -12,5 +12,8 @@ userRouter.get("/refresh", updateAccessToken)
 userRouter.get('/me', isAuthenticated, getUserInfo)
 userRouter.post("/social-auth", socialAuth)
 userRouter.put("/update-user-info", isAuthenticated, updateUserInfo)
+userRouter.put("/update-user-password", isAuthenticated, updatePassword)
+userRouter.put("/update-user-avatar", isAuthenticated, updateProfilePicture)
+
 
 export default userRouter;
