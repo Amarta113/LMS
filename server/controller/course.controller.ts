c022f7ff3ee774782b7b143ac97c8a3496f99f4a
Amarta113 <amartawaghani@gmail.com>
2026-09-20 21:14:43 +0500

    add: implement uploadCourse function to handle course thumbnail uploads

---
+++ server/controller/course.controller.ts
@@ -0,0 +1,27 @@
+import type { NextFunction, Request, Response } from "express";
+import { CatchAsyncError } from "../middleware/catchAsyncError";
+import { ErrorHandler } from '../utils/ErrorHandler';
+import cloudinary from "../utils/cloudinary";
+import { createCourse } from "../services/course.server";
+
+// upload 
+export const uploadCourse = CatchAsyncError(
+    async (req: Request, res: Response, next: NextFunction) => {
+    try{
+        const data = req.body
+        const thumbnail = data.thumbnail
+        if(thumbnail){
+            const myCloud = await cloudinary.uploader.upload(thumbnail, {
+                folder: "courses"
+            })
+            data.thumbnail = {
+                public_id: myCloud.public_id,
+                url: myCloud.secure_url
+            }
+        }
+
+        createCourse(data, res, next)
+    }catch(error:any){
+        return next(new ErrorHandler(error.message, 400))   
+    }
+})
