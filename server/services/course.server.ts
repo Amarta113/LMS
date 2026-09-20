57b451153ae27dee6457777f0cae3a971f14cc02
Amarta113 <amartawaghani@gmail.com>
2026-09-20 21:14:30 +0500

    add: implement createCourse function in course service

---
+++ server/services/course.server.ts
@@ -0,0 +1,18 @@
+import type { NextFunction, Request, Response } from "express";
+import CourseModel from "../models/course.model"
+import { CatchAsyncError } from "../middleware/catchAsyncError"
+import { ErrorHandler } from "../utils/ErrorHandler"
+
+export const createCourse = CatchAsyncError(
+    async (data: any, res: Response, next: NextFunction) => {
+    try{
+        const course = await CourseModel.create(data)
+        res.status(201).json({
+            success: true,
+            course
+        })
+    }catch(error:any){
+        return next(new ErrorHandler(error.message, 400))   
+    }
+}
+)
