9628462dd8f70de3634462ca45476c10d6221589
Amarta113 <amartawaghani@gmail.com>
2026-09-22 20:33:47 +0500

    add: include getAllCourses function in course routes

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,10 +1,11 @@
 import express from 'express'
-import { editCourse, getSingleCourse, uploadCourse } from '../controller/course.controller'
+import { editCourse, getAllCourses, getSingleCourse, uploadCourse } from '../controller/course.controller'
 import { authorizeRoles, isAuthenticated } from '../middleware/auth'
 
 const courseRouter = express.Router()
 courseRouter.post('/create-course', isAuthenticated, authorizeRoles("admin"), uploadCourse)
 courseRouter.put('/edit-course/:id', isAuthenticated, authorizeRoles("admin"), editCourse)
 courseRouter.get('/get-single-course/:id', getSingleCourse)
+courseRouter.get('/get-all-courses', getAllCourses)
 
 export default courseRouter
