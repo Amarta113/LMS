704f95e7bd2e3a2c15db775f94cf7e2e2908f72b
Amarta113 <amartawaghani@gmail.com>
2026-09-23 20:55:58 +0500

    feat: add reply functionality to course reviews

--- server/controller/course.controller.ts
+++ server/controller/course.controller.ts
@@ -294,3 +294,37 @@ export const addReview = CatchAsyncError(async (req: Request, res: Response, nex
         return next(new ErrorHandler(error.message, 400))   
     }
 })
+
+// add reply in review
+interface IAddReviewData{
+    comment: string;
+    courseId: string;
+    reviewId: string;
+}
+export const addReplyToReview = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const {comment, courseId, reviewId} = req.body as IAddReviewData
+        const course = await CourseModel.findById(courseId)
+        if(!course){
+            return next(new ErrorHandler("Course not found", 400))
+        }
+        const review = course?.reviews.find((rev: any) => rev._id.toString() === reviewId)
+        if(!review){
+            return next(new ErrorHandler("Review not found", 404))
+        }
+        const replyData: any = {
+            user: req.user,
+            comment
+        }
+
+        course.reviews.push(replyData)
+        await course?.save()
+        res.status(200).json({
+            success: true,
+            course
+        })
+
+    } catch (error:any) {
+        return next(new ErrorHandler(error.message, 400))   
+    }
+})
