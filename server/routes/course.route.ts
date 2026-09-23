e3219d5d017f0c7a55617e06c8eba671aa0c586b
Amarta113 <amartawaghani@gmail.com>
2026-09-23 16:30:25 +0500

    fix: import addAnswer function in course routes for answer submission

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addQuestion, editCourse, getAllCourses, getCourseByUser, getSingleCourse, uploadCourse } from '../controller/course.controller'
+import { addAnswer, addQuestion, editCourse, getAllCourses, getCourseByUser, getSingleCourse, uploadCourse } from '../controller/course.controller'
 import { authorizeRoles, isAuthenticated } from '../middleware/auth'
 
 const courseRouter = express.Router()
@@ -9,5 +9,6 @@ courseRouter.get('/get-single-course/:id', getSingleCourse)
 courseRouter.get('/get-all-courses', getAllCourses)
 courseRouter.get('/get-course-content/:id', isAuthenticated, getCourseByUser)
 courseRouter.put('/add-question', isAuthenticated, addQuestion)
+courseRouter.put('/add-answer', isAuthenticated, addAnswer)
 
 export default courseRouter
